304017ad6cb63825ec8113564ffe3048da2e9d7c
Jeff Hykin <jeff.hykin@gmail.com>
2019-12-03 19:53:33 -0600

    -

--- setup.rb
+++ setup.rb
@@ -5,5 +5,5 @@ if not Console.has_command("python3")
     system "atk run jeff-hykin/install-python"
 end
 
-system "pip install opencv-python"
-system "pip install numpy"
+system "pip3 install opencv-python"
+system "pip3 install numpy"
