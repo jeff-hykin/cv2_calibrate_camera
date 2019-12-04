b3eae84ff896ff00b07b857b09b508b0d45dbfe5
Jeff Hykin <jeff.hykin@gmail.com>
2019-12-03 20:13:16 -0600

    -

--- main.py
+++ main.py
@@ -37,6 +37,23 @@ for file_name in images:
         print('distortion_coefficients = ', distortion_coefficients)
         cv2.waitKey(5000)
         
+        mtx = camera_matrix
+        dist = distortion_coefficients
+        
+        # 
+        # undistort
+        # 
+        # img = cv2.imread('left12.jpg') # not sure why guy used this one
+        h,  w = img.shape[:2]
+        newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx,dist,(w,h),1,(w,h))
+        
+        # undistort
+        dst = cv2.undistort(img, mtx, dist, None, newcameramtx)
+
+        # crop the image
+        x,y,w,h = roi
+        dst = dst[y:y+h, x:x+w]
+        cv2.imwrite('calibresult.png',dst)
         
 
 cv2.destroyAllWindows()
